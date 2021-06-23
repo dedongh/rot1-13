5af2dc0e02ade009fe7e6bc59feacadf84437871
braemma <asiamahemma95@gmail.com>
2021-06-23 04:58:32 +0000

    Initial Commit

---
+++ ROT13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ROT13
+//
+//  Created by Bra Emma on 23/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ROT13/ROT13App.swift
@@ -0,0 +1,17 @@
+//
+//  ROT13App.swift
+//  ROT13
+//
+//  Created by Bra Emma on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ROT13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
